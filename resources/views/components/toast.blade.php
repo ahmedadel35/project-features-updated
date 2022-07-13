8e67a364bc424ed3670386abda88616fd4c10410
abo3adel <abo3adel35@gmail.com>
2022-07-14 00:28:29 +0100

    ui restyling toast componsent to add notifications

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,45 +1,68 @@
 <div x-data="{
     notices: [],
-    visible: [],
     add(notice) {
         notice.id = Date.now()
-        if (notice.type === 'error') {
-            notice.text = '😵 ' + notice.text
-        }else if (notice.type === 'warning') {
-            notice.text = '⚡ ' + notice.text
-        }else if (notice.type === 'info') {
-            notice.text = '🔥 ' + notice.text
-        }else {
-            notice.text = '🔥 ' + notice.text
-        }
         this.notices.push(notice)
-        this.fire(notice.id)
-    },
-    fire(id) {
-        this.visible.push(this.notices.find(notice => notice.id == id))
-        const timeShown = 2000 * this.visible.length
+
+        const timeShown = 2000 * this.notices.length
         setTimeout(() => {
-            this.remove(id)
+            this.remove(notice.id)
         }, timeShown)
     },
     remove(id) {
-        this.visible.splice(this.visible.findIndex(notice => notice.id == id), 1)
+        this.notices.splice(this.notices.findIndex(notice => notice.id == id), 1)
     },
 }">
-<div class="fixed bottom-0 p-3 toast-container ltr:right-0 rtl:left-0" x-on:toast.window="add($event.detail)">
-    <template x-for="notice of notices" :key="notice.id">
-        <div class="flex items-center w-full max-w-xs px-2 py-3 my-3 text-white rounded-lg shadow" role="alert" x-show="visible.includes(notice)" x-bind:class="{
-            'bg-green-600': notice.type === 'success',
-            'bg-blue-600': notice.type === 'info',
-            'bg-orange-600': notice.type === 'warning',
-            'bg-red-600': notice.type === 'error',
-        }">
-            <div class="text-sm font-normal ltr:mr-3 rtl:ml-3" x-text="notice.text"></div>
-            <button type="button" class="inline-flex items-center justify-center w-8 h-8 p-0 text-center text-white bg-red-600 rounded-lg bg-opacity-80 focus:ring-2 focus:ring-red-300 hover:bg-opacity-100 focus:outline-none" aria-label="Close" x-on:click.prevent="remove(notice.id)">
-                <span class="sr-only" >Close</span>
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </button>
-        </div>
-    </template>
- </div>
+    <div class="fixed bottom-5 p-3 toast-container ltr:right-5 rtl:left-5" x-on:toast.window="add($event.detail)">
+        <template x-for="notice of notices" :key="notice.id">
+            <div id="toast-notification"
+                class="p-2 w-full max-w-xs text-gray-900 bg-white rounded-lg shadow dark:bg-gray-800 dark:text-gray-300 cursor-pointer mb-2"
+                role="alert">
+                <div class="flex items-center relative" x-on:click.prevent="remove(notice.id)">
+                    <div class="inline-block relative shrink-0 text-white">
+                        <template x-if="notice.img">
+                            <div>
+                                <img class="w-12 h-12 rounded-full" x-bind:src="notice.img"
+                                    alt="user profile image" />
+                                <span
+                                    class="inline-flex absolute right-0 bottom-0 justify-center items-center w-6 h-6 bg-blue-600 rounded-full">
+                                    <x-fas-fire class="!w-4 !h-4 !m-0" />
+                                </span>
+                            </div>
+                        </template>
+
+                        <template x-if="!notice.img">
+                            <div class="inline-flex flex-shrink-0 justify-center items-center w-8 h-8  rounded-lg has-gr"
+                                x-bind:class="{
+                                    'green': notice.type === 'success',
+                                    'red': notice.type === 'error',
+                                    'lime': notice.type === 'warning',
+                                    'cyan': notice.type === 'info',
+                                }">
+                                <template x-if="notice.type === 'success'">
+                                    <x-fas-check class="!w-4 !h-4 !m-0" />
+                                </template>
+                                <template x-if="notice.type !== 'success'">
+                                    <x-fas-times class="!w-4 !h-4 !m-0" />
+                                </template>
+                            </div>
+                        </template>
+                    </div>
+                    <div class="ml-3 text-sm font-normal">
+                        <h4 class="text-sm font-semibold" x-bind:class="{
+                            'text-green-600 dark:text-green-500': notice.type === 'success',
+                            'text-red-600 dark:text-red-500': notice.type === 'error',
+                            'text-orange-600 dark:text-orange-500': notice.type === 'warning',
+                            'text-cyan-600 dark:text-cyan-500': notice.type === 'info',
+                        }" x-text="notice.text"></h4>
+                        <div class="text-sm font-normal text-gray-600 dark:text-gray-400" x-text="notice.info">
+                        </div>
+                        <span class="text-xs font-medium text-blue-600 dark:text-blue-500">
+                            {{__('nav.few_ago')}}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </template>
+    </div>
 </div>
