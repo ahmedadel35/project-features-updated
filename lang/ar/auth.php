8056e407f41ef9a4a93b0e928d67a394651db942
abo3adel <abo3adel35@gmail.com>
2022-07-17 13:59:06 +0100

    add arabic default langauge files

---
+++ lang/ar/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+  'BeforeBroccesing' => 'قبل المتابعة ، يرجى التحقق من بريدك الإلكتروني للحصول على رابط التحقق  ',
+  'Confirm-Password' => 'تأكيد كلمة المرور',
+  'E-Mail-Address' => 'عنوان البريد الإلكتروني',
+  'Forgot-Your-Password' => 'نسيت كلمة المرور؟',
+  'Forgot-Your-Password?' => 'نسيت كلمة المرور؟',
+  'If-you-did-not-receive-the-email' => 'إذا لم تستلم البريد الإلكتروني',
+  'Login' => 'تسجيل الدخول',
+  'Name' => 'الاسم',
+  'Password' => 'كلمة المرور',
+  'Register' => 'تسجيل',
+  'Remember-Me' => 'تذكرني',
+  'Reset-Password' => 'إعادة تعيين كلمة المرور',
+  'Send-Password-Reset-Link' => 'إرسال رابط إعادة تعيين كلمة المرور',
+  'Verify-Your-Email-Address' => 'تحقق من عنوان بريدك الإلكتروني
+  ',
+  'click-here-to-request-another' => 'انقر هنا لطلب آخر
+  ',
+  'confirmYourEmail' => 'يرجى تأكيد كلمة المرور الخاصة بك قبل المتابعة',
+  'failed' => 'بيانات الاعتماد هذه غير متطابقة مع البيانات المسجلة لدينا.',
+  'freshEmailWasSent' => '',
+  'throttle' => 'عدد كبير جدا من محاولات الدخول. يرجى المحاولة مرة أخرى بعد :seconds ثانية.',
+);
