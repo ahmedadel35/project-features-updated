e92bdb1126b06badce997cd4e1d5dddc5453e65a
abo3adel <abo3adel35@gmail.com>
2022-07-19 19:27:10 +0100

    styling categories list

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -1,4 +1,4 @@
-<x-card :id="$cat->slug" :url="route('categories.show', $cat->slug)" class="w-full {{$class ?? 'sm:w-1/2 md:w-1/3 p-3 sm:px-2 md:px-4'}}">
+<x-card :id="$cat->slug" :url="route('categories.show', $cat->slug)" class="w-full p-3 sm:w-1/2 md:w-1/3 lg:w-1/4 sm:px-2 md:px-4">
     <x-slot name='title'>
         {{ $cat->title }}
     </x-slot>
@@ -8,20 +8,23 @@
             {{ $cat->description }}
         </p>
         <div class="px-2 py-4 text-blue-600 rounded dark:text-blue-400">
-            {{__('nav.projects')}}: {{ $cat->projects_count }}
+            {{ __('nav.projects') }}: {{ $cat->projects_count }}
         </div>
     </x-slot>
 
     <x-slot name='footer'>
         @isset($canEdit)
-        <button class="btn cyan" x-data type="button" x-on:click.prevent="$dispatch('category-modal', {
+            <button class="mx-1 btn cyan" x-data type="button"
+                x-on:click.prevent="$dispatch('category-modal', {
             editMode: true,
             slug: '{{ $cat->slug }}',
             name: '{{ $cat->title }}',
             desc: '{{ $cat->description }}',
         })">
                 <x-fas-pencil />
-                {{ __('category.edit') }}
+                <span class="sm:hidden lg:inline-block">
+                    {{ __('category.edit') }}
+                </span>
             </button>
         @endisset
         <x-btn-delete :url="route('categories.destroy', $cat->slug)" :id="$cat->slug" />
