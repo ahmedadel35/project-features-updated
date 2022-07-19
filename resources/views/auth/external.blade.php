a0f3aad842cb768d0fd0fc2d3a5157cfe79c171d
abo3adel <abo3adel35@gmail.com>
2022-07-19 12:04:40 +0100

    styling extranal login buttons

--- resources/views/auth/external.blade.php
+++ resources/views/auth/external.blade.php
@@ -1,10 +1,10 @@
 <div class="mt-3 py-3 flex items-center justify-between border-t border-gray-500">
     <x-btn-with-spinner tag='a' href="{{ route('ext-login.github.redirect') }}"
-        desc='login using github' class="bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50" style="background-image: none" icon='fab-github'>
+        desc='login using github' class="github bg-none" icon='fab-github'>
         <span>{{__('auth.joinas')}} {{__('auth.github')}}</span>
     </x-btn-with-spinner>
     <x-btn-with-spinner tag='a' href="{{ route('ext-login.facebook.redirect') }}"
-        desc='login using facebook' class="bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50" style="background-image: none" icon='fab-facebook'>
+        desc='login using facebook' class="bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 bg-none" icon='fab-facebook'>
         <span>{{__('auth.joinas')}} {{__('auth.facebook')}}</span>
     </x-btn-with-spinner>
 </div>
